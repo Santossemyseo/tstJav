9020302fc0106cf51889535b59e76717b0472d2d
Santossemyseo <99697819+Santossemyseo@users.noreply.github.com>
2022-07-17 15:41:53 -0500

    <esf

--- js/cof.js
+++ js/cof.js
@@ -3,7 +3,7 @@ const mail = document.getElementById("email")
 const abut = document.getElementById("but")
 const asub = document.getElementById("sub")
 
-asub.addEventListener("click",()=>{
+asub.addEventListener("click",(e)=>{
     alert("hi");
 })
 
