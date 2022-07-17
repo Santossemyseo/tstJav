bd619c3040733f374fa383998b1e83203e05904f
Santossemyseo <99697819+Santossemyseo@users.noreply.github.com>
2022-07-17 13:44:15 -0500

    dshgs

--- js/cof.js
+++ js/cof.js
@@ -3,7 +3,7 @@
 // img.addEventListener("error",()=>{
 //     alert("")
 // })
-const name = document.getElementById("name");
+const nombre = document.getElementById("nombre");
 const mail = document.getElementById("email");
 const but = document.getElementById("but");
 const sub = document.getElementById("sub");
