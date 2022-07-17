c8199e88de1413ab664eda67796932372a441b90
Santossemyseo <99697819+Santossemyseo@users.noreply.github.com>
2022-07-17 13:31:57 -0500

    test

--- js/cof.js
+++ js/cof.js
@@ -1,5 +1,10 @@
 let img = document.querySelector(".im");
 
 img.addEventListener("error",()=>{
-    alert("df")
+    alert("")
 })
+cons name = document.getElementById("1");
+cons mail = document.getElementById("2");
+cons sub = document.getElementById("3");
+cons but = document.getElementById("4");
+
