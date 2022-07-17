d4d96f8a169590da0739e2cb5fe5188fb49124dc
Santossemyseo <99697819+Santossemyseo@users.noreply.github.com>
2022-07-17 14:03:06 -0500

    zvfd

--- js/cof.js
+++ js/cof.js
@@ -1,5 +1,4 @@
-
-const nombre = document.getElementById(nombre);
+const nombre = document.getElementById("nombre");
 const mail = document.getElementById("email");
 const but = document.getElementById("but");
 const sub = document.getElementById("sub");
