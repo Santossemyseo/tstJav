82a0c9bd58b43df335485a1ca0cbec454bbfcaff
Santossemyseo <99697819+Santossemyseo@users.noreply.github.com>
2022-07-17 14:05:28 -0500

    fj

--- js/cof.js
+++ js/cof.js
@@ -1,7 +1,7 @@
-const nombre = document.getElementById("nombre");
+const ombre = document.getElementById("nombre");
 const mail = document.getElementById("email");
-const but = document.getElementById("but");
-const sub = document.getElementById("sub");
+const abut = document.getElementById("but");
+const asub = document.getElementById("sub");
 
 
 
