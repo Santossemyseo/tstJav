616eb8f7feb5e81150072b4dd2dce1ca9d7dc45e
Santossemyseo <99697819+Santossemyseo@users.noreply.github.com>
2022-07-16 22:36:28 -0500

    zx

--- js/cof.js
+++ js/cof.js
@@ -1,3 +1,4 @@
 const button = document.querySelector(".button");
-
-button.addEventListener("click",(e)=>{alert(" aca esta ");});
+button.addEventListener("click",(e)=>{
+    alert("aca esta ")
+});
